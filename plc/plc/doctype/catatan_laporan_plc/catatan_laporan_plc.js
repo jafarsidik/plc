99382872b7c41861952c9f229918faf69d69104d
Jafarsidik <jeff.sidik@gmail.com>
2023-08-25 17:14:46 +0700

    update

---
+++ plc/plc/doctype/catatan_laporan_plc/catatan_laporan_plc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jeff.sidik@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Catatan Laporan PLC', {
+	// refresh: function(frm) {
+
+	// }
+});
