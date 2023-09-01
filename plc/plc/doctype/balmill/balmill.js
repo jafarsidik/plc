17cf75002b1e09e45c4cb6209730f108e0f0f53f
Jafarsidik <jeff.sidik@gmail.com>
2023-09-01 07:37:02 +0700

    g

---
+++ plc/plc/doctype/balmill/balmill.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, jeff.sidik@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Balmill', {
+	// refresh: function(frm) {
+
+	// }
+});
